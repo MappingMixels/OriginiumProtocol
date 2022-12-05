08c83ea51c53a185d34a00c7cbe4bc5f62161759
TT432 <2437315224@qq.com>
2022-12-05 19:46:41 +0800

    引入依赖，完善 prts

--- src/main/java/com/elfmc/ys/originiumprotocol/OriginiumProtocol.java
+++ src/main/java/com/elfmc/ys/originiumprotocol/OriginiumProtocol.java
@@ -1,30 +1,12 @@
 package com.elfmc.ys.originiumprotocol;
 
-<<<<<<< ca892b2be6baef236b1d7d707667311d3713c696
-import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
-=======
 import com.elfmc.ys.originiumprotocol.common.item.OPItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
->>>>>>> first item
-
-import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * @author DustW
@@ -35,12 +17,12 @@ public class OriginiumProtocol {
 
     public static final CreativeModeTab TAB = new CreativeModeTab(ID) {
         @Override
-        public ItemStack makeIcon() {
+        public @NotNull ItemStack makeIcon() {
             return new ItemStack(OPItems.PRTS.get());
         }
     };
 
-    public OriginiumProtocol() {
+    private OriginiumProtocol() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
         OPItems.ITEMS.register(bus);
